15697665adca340b2cdc7267627c58dfa3a0ad3b
digachandra <imdigachandra@gmail.com>
2016-09-30 16:08:35 +0700

    Update "/template"

--- routes/template.js
+++ routes/template.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('template/content_signin');
 });
 
+router.post('/', function(req, res, next) {
+  res.redirect('/template/hunt');
+});
+
+router.get('/signup', function(req, res, next) {
+  res.render('template/content_signup');
+});
+
+router.get('/signout', function(req, res, next) {
+  res.redirect('/template');
+});
+
+router.get('/hunt', function(req, res, next) {
+  res.render('template/content_dashboard_hunt');
+});
+
+router.get('/prey', function(req, res, next) {
+  res.render('template/content_dashboard_prey');
+});
+
 module.exports = router;
